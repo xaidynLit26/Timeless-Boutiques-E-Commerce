eb02178d45d03a47c0b0a591edf6b3c89aaab456
Xamarys Liranzo <xamarys.liranzo456@gmail.com>
2025-07-06 14:16:57 -0400

    Delete flowers directory

--- flowers/Database/user.sql
+++
@@ -1,8 +0,0 @@
-USE timeless_boutiques;
-
-INSERT INTO users (id, fname, lname, email, password, address, city, state, zip_code, phone) 
-VALUES 
-(1, 'John', 'Smith', 'johnsmith@gmail.com', '123', '15 East 40th Street', 'Bayonne', 'New Jersey', '07002', '123-456-7890');
-SELECT * FROM users WHERE email = 'johnsmith@gmail.com';
-
-SELECT*FROM users;
--- flowers/logout.php
+++
@@ -1,9 +0,0 @@
-<!--THis php ensures that when the user logouts out all of their data 
-is cleared from the session array-->
-<?php
-    session_start();
-    $_SESSION = array();
-    session_destroy();
-    header("location: login.php");
-    exit;
-?>
